e99c4597b605271f26a719dfd86f78b8b3f7be6b
silend <solokhinii@gmail.com>
2014-12-21 18:57:09 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IMDepot::Application.config.secret_key_base = '6f2246e32acafd0921fb1d47df542d329e83ad78dc8f163457bb716556ce7d80f15f855e1709d13a5152603a9da9a85a2b4247ce6308e15378b82f0b821e34ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IMDepot::Application.config.session_store :cookie_store, key: '_IMDepot_session'
